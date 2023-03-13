9402a18141aa636bd184fb8c4d917eaecab60f7a
lavrric <mlavric64@gmail.com>
2023-03-13 09:08:05 +0200

    test(): addStudent basic tests JUnit

--- src/test/java/ro/ubb/MainTest.java
+++ src/test/java/ro/ubb/MainTest.java
@@ -1,14 +1,56 @@
 package ro.ubb;
 
 import org.junit.jupiter.api.Test;
+import ro.ubb.domain.Assignment;
+import ro.ubb.domain.Grade;
+import ro.ubb.domain.Student;
+import ro.ubb.repository.AssignmentXMLRepository;
+import ro.ubb.repository.GradeXMLRepository;
+import ro.ubb.repository.StudentXMLRepository;
+import ro.ubb.service.Service;
+import ro.ubb.validation.AssignmentValidator;
+import ro.ubb.validation.GradeValidator;
+import ro.ubb.validation.StudentValidator;
+import ro.ubb.validation.Validator;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest
 {
+
+    Validator<Student> studentValidator = new StudentValidator();
+    Validator<Assignment> assignmentValidator = new AssignmentValidator();
+    Validator<Grade> gradeValidator = new GradeValidator();
+
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "students_test.xml");
+    AssignmentXMLRepository fileRepository2 = new AssignmentXMLRepository(assignmentValidator, "assignments_test.xml");
+    GradeXMLRepository fileRepository3 = new GradeXMLRepository(gradeValidator, "grades_test.xml");
+
+    Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    void doesAddStudentWork(){
+        int isSuccessful = service.saveStudent("1", "Numele", 422);
+        assertEquals(0, isSuccessful);
+    }
+
+    @Test
+    void doesAddStudentWorkProperly(){
+        service.saveStudent("1", "Numele", 422);
+        Iterable<Student> students = service.findAllStudents();
+        Student student = students.iterator().next();
+
+        assertEquals(student.getID(), "1");
+        assertEquals(student.getName(), "Numele");
+        assertEquals(student.getGroup(), 422);
     }
 }
